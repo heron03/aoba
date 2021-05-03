8a0d91afa3a176db30364946c45488691ebc5cd2
heron03 <35074726+heron03@users.noreply.github.com>
2021-05-03 19:52:56 -0300

    Ações do App Controller

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -43,11 +43,62 @@ class AppController extends Controller
 
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        $this->viewBuilder()->setLayout('bootstrap');
+        $this->viewBuilder()->setHelpers(['Js']); 
+    }
 
-        /*
-         * Enable the following component for recommended CakePHP form protection settings.
-         * see https://book.cakephp.org/4/en/controllers/components/form-protection.html
-         */
-        //$this->loadComponent('FormProtection');
+    public function index() {
+        $entity = $this->{$this->getModelName()}->newEmptyEntity();
+        $this->set(compact('entity'));
+        $this->setPaginateConditions();
+        try {
+            $this->set($this->getControllerName(), $this->paginate());        
+        } catch (NotFoundException $e) {
+            $this->redirect('/' . $this->getControllerName());
+        }        
+    }
+
+    public function add() {
+        $entity = $this->{$this->getModelName()}->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $entity = $this->{$this->getModelName()}->patchEnity($entity, $this->request->getData());
+            if ($this->{$this->getModelName()}->save($entity)) {
+                $this->Flash->bootstrap('Gravado com sucesso!', array('key' => 'success'));
+                $this->redirect(['action' => 'index']);
+            }
+        }
+        $this->set(compact('entity'));
+    }
+
+    public function edit($id = null) {
+        $entity = $this->getEditEntity($id);
+        if ($this->request->is(['post', 'patch', 'put'])) {
+            $entity = $this->{$this->getModelName()}->patchEnity($entity, $this->request->getData());
+            if ($this->{$this->getModelName()}->save($entity)) {
+                $this->Flash->bootstrap('Alterado com sucesso!', array('key' => 'success'));
+                $this->redirect(['action' => 'index']);
+            }
+        }
+        $this->set(compact('entity'));
+    }
+
+    public function view($id = null) {
+        $entity = $this->getEditEntity($id);
+        $this->set(compact('entity'));
+    }
+
+    public function delete($id) {
+        $entity = $this->getEditEntity($id);
+        $this->{$this->getModelName()}->delete($entity);
+        $this->Flash->bootstrap('Excluído com sucesso!', array('key' => 'warning'));
+        $this->redirect(['action' => 'index']);
+    }
+    
+    public function getControllerName() {
+        return \Cake\Utility\Inflector::underscore($this->request->getParam('controller'));
+    }
+
+    public function getModelName() {
+        return $this->request->getParam('controller');
     }
 }
